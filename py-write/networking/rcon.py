51e5ee776b8364a267c8e43d24030a679a6e11e3
tawoodward <software@tawoodward.com>
2024-02-21 18:00:56 -0500

    Finished rcon class and functions.

--- py-write/networking/rcon.py
+++ py-write/networking/rcon.py
@@ -17,7 +17,7 @@ class RconClient:
         self._writer = None
 
     async def __connect__(self):
-        if not self.writer:
+        if not self._writer:  # Corrected the attribute name
             self._reader, self._writer = await asyncio.open_connection(self.host, self.port)
             await self._login()
         
@@ -30,15 +30,40 @@ class RconClient:
     async def _read(self, len_b):
         data = b''
         while len(data) < len_b:
-            data += await self._reader(len_b, len(data))
-        
+            data += await self._reader.read(len_b - len(data))  # Corrected the method name
         return data
 
-
-    async def _send(self, type, cmd):
-        packet = struct.pack('<i', (time.time()/100)) \
-             + struct.pack('<i', type) + cmd
+    async def _send(self, type, cmd: str):
+        out_id = struct.pack('<i', (time.time()/100))
+        out_packet = out_id \
+             + struct.pack('<i', type) + cmd.encode('utf8')
         
-        self._writer.write(struct.pack('<i', len(packet) + packet + b'/x00/x00'))
+        self._writer.write(struct.pack('<i', len(out_packet) + out_packet + b'/x00/x00'))
 
         response = struct.unpack('<i', await self._read(4))
+        in_packet = await self._read_data(response[0])
+
+        data = verify(out_id, in_packet)  # Using data directly for further processing
+        
+        if not data:
+            raise RuntimeError("Verification failed")  # Or any appropriate exception
+        else:
+            return data.decode('utf8')
+
+    async def send(self, cmd):
+        result = await self._send(2, cmd)
+        return result
+    
+
+def verify(out_id, in_pkt):
+    in_id, in_type = struct.unpack('<ii', in_pkt[:8])
+    in_data, in_padd = in_pkt[8:-2], in_pkt[-2:]
+
+    if in_id != out_id:
+        raise ValueError("Mismatched IDs")  # Or any appropriate exception
+    if in_type == -1:
+        raise ValueError("Incorrect password")  # Or any appropriate exception
+    if in_padd != b'/x00/x00':
+        raise ValueError("Incorrect padding")  # Or any appropriate exception
+
+    return in_data
